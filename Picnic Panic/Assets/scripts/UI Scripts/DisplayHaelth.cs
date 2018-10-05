1818f9f549c9652b375b85a9c09706102b85f73d
Veanoid <36467761+Veanoid@users.noreply.github.com>
2018-10-05 15:29:28 +1000

    -Added Pie King Health to UI -Added Player Health to UI

---
+++ Picnic Panic/Assets/scripts/UI Scripts/DisplayHaelth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayHaelth : MonoBehaviour {
+
+    public Text[] displayHealth;
+    public Actor[] Health;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        for (int i = 0; i < Health.Length; i++)
+        {
+
+            displayHealth[i].text = Health[i].Health.ToString();
+        }
+	}
+}
