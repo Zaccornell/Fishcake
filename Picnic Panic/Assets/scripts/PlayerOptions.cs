1b5d319d7fd52bc068baa29bb4846692160749e8
JohnPlantGit <41980810+JohnPlantGit@users.noreply.github.com>
2018-10-12 16:38:59 +1100

    -Added base PlayerOptions singleton

---
+++ Picnic Panic/Assets/scripts/PlayerOptions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOptions
+{
+    private static readonly PlayerOptions m_instance = new PlayerOptions();
+
+    static PlayerOptions()
+    {
+
+    }
+    private PlayerOptions()
+    {
+
+    }
+
+    public static PlayerOptions Instance
+    {
+        get { return m_instance; }
+    }
+}
