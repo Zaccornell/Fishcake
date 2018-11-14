1dec871534ede7fb93a438749b5f7a5f00779d5d
Veanoid <36467761+Veanoid@users.noreply.github.com>
2018-11-15 10:11:57 +1100

    - Fixed Time scale on reset

--- Picnic Panic/Assets/scripts/UI Scripts/PauseMenu.cs
+++ Picnic Panic/Assets/scripts/UI Scripts/PauseMenu.cs
@@ -102,6 +102,7 @@ public class PauseMenu : MonoBehaviour
         //    XInputDotNetPure.GamePad.SetVibration((XInputDotNetPure.PlayerIndex)current.m_playerNumber - 1, 0, 0); //. set the vibration stregnth 
         //}
         SceneManager.LoadScene(1);
+        Time.timeScale = 1;
     }
 
     /*
