55503d7fd177440c8637479e7df3b7438b2a5931
JohnPlantGit <41980810+JohnPlantGit@users.noreply.github.com>
2018-11-28 15:55:20 +1100

    -Added death particles to the Cockroach -Made death particles destroy themselves when they finish

---
+++ Picnic Panic/Assets/scripts/DestroyParticleEffect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyParticleEffect : MonoBehaviour
+{
+    ParticleSystem[] m_particleSystem;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_particleSystem = GetComponentsInChildren<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        bool destroy = true;
+        foreach (ParticleSystem current in m_particleSystem)
+        {
+            if (current.IsAlive())
+            {
+                destroy = false;
+                break;
+            }
+        }
+
+		if (destroy)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
