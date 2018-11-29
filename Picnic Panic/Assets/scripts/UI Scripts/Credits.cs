f8def1a2384a5674a8eefd951ee6b4d0310a08aa
JohnPlantGit <41980810+JohnPlantGit@users.noreply.github.com>
2018-11-30 10:47:42 +1100

    -Added scrollbar to the credits

---
+++ Picnic Panic/Assets/scripts/UI Scripts/Credits.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Credits : MonoBehaviour
+{
+    public Scrollbar m_scrollbar;
+    public float m_offset;
+
+    private Vector3 m_originalPosition;
+	// Use this for initialization
+	void Start ()
+    {
+        m_originalPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 newPos = m_originalPosition;
+        newPos.y += m_offset * m_scrollbar.value;
+        transform.position = newPos;
+	}
+}
