9cdee3d939546416d995e9f8a9da05fec5540f7f
Veanoid <36467761+Veanoid@users.noreply.github.com>
2018-11-30 11:17:52 +1100

    - Locked moused on MainMenu

--- Picnic Panic/Assets/scripts/UI Scripts/MainMenu.cs
+++ Picnic Panic/Assets/scripts/UI Scripts/MainMenu.cs
@@ -22,13 +22,14 @@ public class MainMenu : MonoBehaviour
 	// Use this for initialization
 	void Start ()
     {
-		
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-
+        
 	}
 
     /*
